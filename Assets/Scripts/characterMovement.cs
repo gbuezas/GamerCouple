f8a5751d0d99eccf96b01b6d9b1bd14407b1c640
Liktchay <31039399+Liktchay@users.noreply.github.com>
2017-08-16 00:39:21 -0300

    Movimiento personaje
    Inicio de codeo del movimiento del personaje.

---
+++ Assets/Scripts/characterMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterMovement : MonoBehaviour {
+
+
+    private Rigidbody2D characterRigidbody;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        characterRigidbody = GetComponent<Rigidbody2D>();
+        
+    }
+
+
+    // Update is called once per frame
+    void Update() {
+        
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Apretando un boton " + KeyCode.A);
+            //speed = 10;
+            characterRigidbody.AddForce(movement * speed);
+        }
+  
+        if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("Apretando boton = " + KeyCode.D);
+           // speed = 10;
+            characterRigidbody.AddForce(movement * speed);
+        }
+
+     
+
+    }
+}
