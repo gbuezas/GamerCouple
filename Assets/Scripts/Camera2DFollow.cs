9c0bef62238c073f77df5e094c66864ddeeeae27
Liktchay <31039399+Liktchay@users.noreply.github.com>
2017-08-29 21:53:26 -0300

    Clampeo camara en eje X
    Se codeo el clampeo de la camara en el eje X usando el primer y ultimo sprite con TAG.

--- Assets/Scripts/Camera2DFollow.cs
+++ Assets/Scripts/Camera2DFollow.cs
@@ -11,20 +11,34 @@ namespace UnitySampleAssets._2D
         public float lookAheadFactor = 3;
         public float lookAheadReturnSpeed = 0.5f;
         public float lookAheadMoveThreshold = 0.1f;
-        public float minClamp = 0f;
-        public float maxClamp = Mathf.Infinity;
+        public float minClampY = 0f;
+        public float maxClampY = Mathf.Infinity;
         public float searchTime = 0f;
+        private Transform objMinClampX;
+        private Transform objMaxClampX;
+        public float minClampX = 0f;
+        public float maxClampX = 1f;
+        public Vector3 coordsDerechaCam;
+        public Vector3 coordsIzquierdaCam;
+        public float offsetCamDerecha = 0f;
+        public float offsetCamIzquierda = 0f;
 
         private float offsetZ;
         private Vector3 lastTargetPosition;
         private Vector3 currentVelocity;
         private Vector3 lookAheadPos;
+        private Vector3 derechaCam;
+        private Vector3 izquierdaCam;
+        
         
         private void Awake()
         {
             if (target == null){
                    SetCameraTarget();
-            }            
+            }
+            GetMaxClampX();
+            GetMinClampX();
+
         }
 
 
@@ -34,17 +48,22 @@ namespace UnitySampleAssets._2D
             lastTargetPosition = target.position;
             offsetZ = (transform.position - target.position).z;
             transform.parent = null;
+            izquierdaCam.x = 0f;
+            izquierdaCam.y = 0f;
+            izquierdaCam.z = 0f;
+
         }
 
         // Update is called once per frame
         private void Update()
         {
-
+            //si la camara no tiene target a seguir, lo busca de nuevo.
             if (target == null){
                 FindPlayer();
                 return;
             }
-               
+
+
             // only update lookahead pos if accelerating or changed direction
             float xMoveDelta = (target.position - lastTargetPosition).x;
 
@@ -61,14 +80,29 @@ namespace UnitySampleAssets._2D
 
             Vector3 aheadTargetPos = target.position + lookAheadPos + Vector3.forward*offsetZ;
             Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref currentVelocity, damping);
+            getRightCam();
+            coordsDerechaCam = Camera.main.ScreenToWorldPoint(derechaCam);
+            coordsIzquierdaCam = Camera.main.ScreenToWorldPoint(izquierdaCam);
+            offsetCamIzquierda = coordsIzquierdaCam.x - newPos.x;
+            offsetCamDerecha = coordsDerechaCam.x - newPos.x;
+            
+             /*if ((coordsDerechaCam.x + 3) >= maxClampX)
+             {
+                 newPos = new Vector3(Mathf.Clamp(newPos.x, minClampX, newPos.x), Mathf.Clamp(newPos.y, minClampY, maxClampY), newPos.z);
+             }
+             else
+             {
+                 newPos = new Vector3(newPos.x, Mathf.Clamp(newPos.y, minClampY, maxClampY), newPos.z);
+             }*/
 
-            newPos = new Vector3(newPos.x, Mathf.Clamp(newPos.y, minClamp, maxClamp), newPos.z);
-               
+            newPos = new Vector3(Mathf.Clamp(newPos.x, minClampX - offsetCamIzquierda, maxClampX - offsetCamDerecha), Mathf.Clamp(newPos.y, minClampY, maxClampY), newPos.z);
+
             transform.position = newPos;
 
             lastTargetPosition = target.position;
         }
 
+        //busca el player target para seguimiento de camara
         private void FindPlayer(){
             if(searchTime <= Time.time){
                 GameObject result = GameObject.FindGameObjectWithTag("Player");
@@ -94,6 +128,48 @@ namespace UnitySampleAssets._2D
                 return;
             }
         }
+        
+        private void GetMaxClampX()
+        {
+            Debug.Log("Buscando ultimo sprite con TAG.");
+            GameObject lastSprite = GameObject.FindGameObjectWithTag("LastSprite");
+            if (lastSprite != null)
+            {
+                Debug.Log("Se encontro ultimo sprite con TAG.");
+                objMaxClampX = lastSprite.transform;
+                maxClampX = objMaxClampX.position.x;
+            }
+            else
+            {
+                Debug.LogError("No se encontro ultimo sprite con TAG");
+                return;
+            }              
+        }
+        private void GetMinClampX()
+        {
+            Debug.Log("Buscando primer sprite con TAG.");
+            GameObject firstSprite = GameObject.FindGameObjectWithTag("FirstSprite");
+            if (firstSprite != null)
+            {
+                Debug.Log("Se encontro primer sprite con TAG.");
+                objMinClampX = firstSprite.transform;
+                minClampX = objMinClampX.position.x;
+                
+            }
+            else
+            {
+                Debug.LogError("No se encontro primer sprite con TAG");
+                return;
+            }
+        }
+        private void getRightCam()
+        {
+            derechaCam.x = Camera.main.pixelWidth;
+            derechaCam.y = Camera.main.pixelHeight;
+            derechaCam.z = 0f;
+        }
+        
+
 
     }
 }
